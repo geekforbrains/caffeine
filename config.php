5b7bed03ea39dd34a3baf074c4286f66f7364076
Gavin Vickery <gdvickery@gmail.com>
2011-02-08 17:07:15 -0800

    Removed deprecated debug configs.

--- config.php
+++ config.php
@@ -8,8 +8,4 @@
  */
 define('CAFFEINE_IGNORE_MODULES', ''); // A comma seperated list of modules to ignore when scanning
 define('CAFFEINE_TIMEZONE', 'UTC'); // Default UTC
-define('CAFFEINE_DEBUG', true);
-define('CAFFEINE_DEBUG_VERBOSITY', 3); // 1 = Low, 2 = Medium, 3 = High
-define('CAFFEINE_DEBUG_IGNORE', ''); // A list of comma seperated classes to ignore in debug
-define('CAFFEINE_DEBUG_WATCH', ''); // A list of comma seperated classes to watch, overrides ignore
 define('CAFFEINE_ERROR_REPORTING', E_ALL | E_STRICT);
