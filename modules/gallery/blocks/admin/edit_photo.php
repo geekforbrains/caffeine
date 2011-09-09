8d48648b409f06faf68aca194a0248ac6d443e4c
Gavin Vickery <gdvickery@gmail.com>
2011-09-09 10:05:10 -0700

    Finished basic Photo Gallery module.

---
+++ modules/gallery/blocks/admin/edit_photo.php
@@ -0,0 +1,21 @@
+<div class="area">
+    <h2>Edit Photo</h2>
+    <form method="post" action="<?php echo Router::current_url(); ?>">
+        <ul>
+            <li>
+                <img src="<?php l('media/image/%d/0/125/125', $photo['media_cid']); ?>" />
+            </li>
+            <li class="text small">
+                <label>Title</label>
+                <input type="text" name="title" value="<?php echo $photo['title']; ?>" />
+            </li>
+            <li class="textarea medium">
+                <label>Description</label>
+                <textarea name="description"><?php echo $photo['description']; ?></textarea>
+            </li>
+            <li class="buttons">
+                <input type="submit" value="Update Photo" />
+            </li>
+        </ul>
+    </form>
+</div>
