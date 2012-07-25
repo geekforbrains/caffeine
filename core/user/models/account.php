29c8324276124897ecdc79b7d13ef4c7b6fc9238
Gavin Vickery <gavin@geekforbrains.com>
2012-07-24 18:21:12 -0700

    Cleaning up some changes imported from another project.

--- core/user/models/account.php
+++ core/user/models/account.php
@@ -4,8 +4,6 @@ class User_AccountModel extends Model {
 
     public $_timestamps = true;
 
-    public $_belongsTo = array('plan.plan');
-
     public $_hasMany = array('user.user');
     
     public $_fields = array(
@@ -13,27 +11,9 @@ class User_AccountModel extends Model {
             'type' => 'varchar',
             'length' => 255,
             'not null' => true
-        ),
-        'status' => array( // trial, active, frozen, closed
-            'type' => 'varchar',
-            'length' => 10,
-            'not null' => true
-        ),
-        'trial_ends' => array(
-            'type' => 'int',
-            'size' => 'big',
-            'not null' => true
         )
     );
 
-    public $_indexes = array('subdomain', 'status', 'trial_ends');
-
-    public function getBySubdomain($subdomain) {
-        return $this->where('subdomain', '=', $subdomain)->first();
-    }
-
-    public function isValidSubdomain($subdomain) {
-        return (boolean) $this->where('subdomain', '=', $subdomain)->first();
-    }
+    public $_indexes = array('subdomain');
 
 }
