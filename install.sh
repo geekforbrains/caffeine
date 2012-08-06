417a0d03c586f618e4346ea2d4c249514f74ab77
Gavin Vickery <gavin@geekforbrains.com>
2012-08-06 12:22:35 -0700

    Updating output to be more readable.

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ INSTALL_DIR="$HOME/.caffeine"
 EXPORT_STR='export PATH=$PATH:$HOME/.caffeine # Caffeine Tools'
 
 if [ -d "$INSTALL_DIR" ]; then
-    echo 'Looks like Caffeine Tools are already installed.'
+    echo "Looks like Caffeine Tools are already installed."
     echo "Installation directory: $INSTALL_DIR"
     exit 1;
 fi
@@ -21,7 +21,17 @@ echo "Caffeine Tools were installed to: $INSTALL_DIR"
 
 echo "Adding installation directory to your PATH (~/.bash_profile)"
 echo $EXPORT_STR >> ~/.bash_profile
-source ~/.bash_profile
 
-echo 'All done.'
-echo 'To start using Caffeine, run: caffeine'
+echo "All done."
+echo ""
+echo "Before using Caffeine Tools, you may need to run:"
+echo "$ source ~/.bash_profile"
+echo ""
+echo "If that doesn't work, try closing and re-opening your terminal"
+echo ""
+echo "To start using Caffeine, run:"
+echo "$ caffeine"
+echo ""
+echo "Thanks for using Caffeine!"
+echo "~ Gavin Vickery <gavin@geekforbrains.com>"
+echo ""
